0634b05b7520403d6008e25f874637d6c09fa58e
Ichiro Home DT <ichi.theinterface@gmail.com>
2022-03-20 13:55:23 -0700

    updated on 03-20-2022

--- src/config.js
+++ src/config.js
@@ -24,15 +24,12 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 9,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Skin" },
+      { name: "Eyes" },
+      { name: "Underware" },
     ],
   },
 ];
@@ -42,8 +39,8 @@ const shuffleLayerConfigurations = false;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 1440,
+  height: 1024,
   smoothing: false,
 };
 
