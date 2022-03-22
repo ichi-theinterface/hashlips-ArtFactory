1fd63880a57057aed08a988e64aea25e071930a9
Ichiro Home DT <ichi.theinterface@gmail.com>
2022-03-21 18:27:09 -0700

    updated the config.js on 03-21-2022

--- src/config.js
+++ src/config.js
@@ -24,7 +24,7 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 8,
+    growEditionSizeTo: 2,
     layersOrder: [
       { name: "Background" },
       { name: "Skin" },
